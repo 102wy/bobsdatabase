ab115767dd0224962483253a501089947a18fa55
102 <89123240+102wy@users.noreply.github.com>
2025-10-28 21:25:00 +0900

    이미지 아래 마진 수정

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -43,10 +43,11 @@ export const GlobalStyle = createGlobalStyle`
   }
   img {
     max-width: calc(100vw - 40px);
-    margin-bottom: 20px;
     
   }
-
+  .title {
+    margin-bottom:20px
+  }
   .keyword {
     color: #6d1787;
     font-weight: bold;
